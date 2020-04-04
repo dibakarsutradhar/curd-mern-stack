44f1025f843029e576d8cc59da2124b4c0ee5858
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2020-04-04 20:50:46 +0800

    User Registration with Mongoose

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 
 const app = express();
+
+// Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Database config
 const db = require("./config/keys").mongoURI;
